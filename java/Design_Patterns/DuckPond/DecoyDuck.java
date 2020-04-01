82829ff126676bea7e4181be91af492e6817457b
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-02 00:09:26 +0300

    test

--- java/Design_Patterns/DuckPond/DecoyDuck.java
+++ java/Design_Patterns/DuckPond/DecoyDuck.java
@@ -1,6 +1,6 @@
 package Design_Patterns.DuckPond;
 
-public class DecoyDuck extends Duck implements Quackable{
+public class DecoyDuck extends Design_Patterns.DuckPond.Duck implements Design_Patterns.DuckPond.Quackable {
 
     public void quack() {
         //System.out.println("Silent.");
