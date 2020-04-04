82907fe6998bb6544e9524b5194c150a4ffeb3ca
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-04 15:50:26 +0300

    Duck class will delegate behavior.

--- java/src/DuckPond/Duck.java
+++ java/src/DuckPond/Duck.java
@@ -1,12 +1,18 @@
 package src.DuckPond;
 
+import src.DuckPond.Behaving.FlyBehavior;
+import src.DuckPond.Quacking.QuackBehavior;
+
 abstract public class Duck {
 
     void swim() {
-
     }
 
     abstract void display();
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
 
     public static void main(String[] args) {
         RubberDuck rubberDuck = new RubberDuck();
@@ -21,6 +27,7 @@ abstract public class Duck {
         Rocket rocket = new Rocket();
 
         rocket.fly();
+
     }
 
 }
