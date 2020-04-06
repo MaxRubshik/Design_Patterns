3c583a5ed0c6753c7c7fd9ae69229ebec7965047
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-06 16:16:43 +0300

    DuckSimulator finished.

--- java/src/DuckPond/MiniDuckSimulator.java
+++ java/src/DuckPond/MiniDuckSimulator.java
@@ -2,6 +2,16 @@ package src.DuckPond;
 
 public class MiniDuckSimulator {
     public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+        mallard.performSwim();
 
+        System.out.println();
+
+        Duck rubberDuck = new RubberDuck();
+        rubberDuck.performQuack();
+        rubberDuck.performFly();
+        rubberDuck.performSwim();
     }
 }
