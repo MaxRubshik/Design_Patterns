f05a5703c926842934ef3408105013cd8eb8ccfd
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-07 14:46:15 +0300

    Manok class with methods added.

---
+++ java/src/DuckPond/Manok.java
@@ -0,0 +1,17 @@
+package src.DuckPond;
+
+import src.DuckPond.Behaving.FlyBehavior;
+import src.DuckPond.Behaving.Flyable;
+import src.DuckPond.Quacking.QuackBehavior;
+
+public class Manok implements FlyBehavior, QuackBehavior {
+    @Override
+    public void fly() {
+        System.out.println("No fly");
+    }
+
+    @Override
+    public void quack() {
+        System.out.println("Wee-eeee-ee");
+    }
+}
