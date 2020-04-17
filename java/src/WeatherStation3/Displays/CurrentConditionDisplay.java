9476c83f9ca87a25cdf076928a4141376b23f4b6
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-04-17 13:21:41 +0300

    CurrentConditionsDisplay improved with embedded realization.

--- java/src/WeatherStation3/Displays/CurrentConditionDisplay.java
+++ java/src/WeatherStation3/Displays/CurrentConditionDisplay.java
@@ -1,7 +1,7 @@
 package src.WeatherStation3.Displays;
 
 import src.WeatherStation3.Interfaces.DisplayElement;
-import src.WeatherStation3.Interfaces.Subject;
+import src.WeatherStation3.WeatherData;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -9,29 +9,29 @@ import java.util.Observer;
 public class CurrentConditionDisplay implements DisplayElement, Observer {
     private float temperature;
     private float humidity;
-    private float pressure;
     private Observable observable;
 
-    public CurrentConditionDisplay(Observable observable){
-
+    public CurrentConditionDisplay(Observable observable) {
+        this.observable = observable;
+        observable.addObserver(this);
     }
 
     @Override
     public void display() {
         System.out.println("Current: " + temperature +
                 " F degrees and " + humidity +
-                " %humidity" + " pressure: " + pressure);
+                " %humidity");
     }
-
-    public void update(float temp, float humidity, float pressure) {
-        this.temperature = temp;
-        this.humidity = humidity;
-        this.pressure = pressure;
-        display();
-    }
+    // + " pressure: " + pressure
 
     @Override
-    public void update(Observable o, Object arg) {
-
+    public void update(Observable obs, Object arg) {
+        if (obs instanceof WeatherData) {
+            int zzz; //to avoid code marker.
+            WeatherData weatherData = (WeatherData) obs;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
+            display();
+        }
     }
 }
--- java/src/WeatherStation3/WeatherStation.java
+++ java/src/WeatherStation3/WeatherStation.java
@@ -6,7 +6,7 @@ import src.WeatherStation3.Interfaces.Subject;
 public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
-        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay((Subject) weatherData);
+        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
         weatherData.setMeasurements(1,99,666);
         weatherData.setMeasurements(22,99,777);
         weatherData.setMeasurements(33,99,888);
