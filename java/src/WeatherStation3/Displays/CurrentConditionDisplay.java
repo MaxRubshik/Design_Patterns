a90c1606b54934f6ed563d78d4f88c4264faffb0
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-04-17 16:44:52 +0300

    move to laptop.

--- java/src/WeatherStation3/Displays/CurrentConditionDisplay.java
+++ java/src/WeatherStation3/Displays/CurrentConditionDisplay.java
@@ -9,7 +9,7 @@ import java.util.Observer;
 public class CurrentConditionDisplay implements DisplayElement, Observer {
     private float temperature;
     private float humidity;
-    private Observable observable;
+    Observable observable;
 
     public CurrentConditionDisplay(Observable observable) {
         this.observable = observable;
@@ -17,21 +17,17 @@ public class CurrentConditionDisplay implements DisplayElement, Observer {
     }
 
     @Override
-    public void display() {
-        System.out.println("Current: " + temperature +
-                " F degrees and " + humidity +
-                " %humidity");
-    }
-    // + " pressure: " + pressure
-
-    @Override
-    public void update(Observable obs, Object arg) {
-        if (obs instanceof WeatherData) {
-            int zzz; //to avoid code marker.
-            WeatherData weatherData = (WeatherData) obs;
-            this.temperature = weatherData.getTemperature();
-            this.humidity = weatherData.getHumidity();
+    public void update(Observable o, Object arg) {
+        if(o instanceof WeatherData){
+            WeatherData weatherData = (WeatherData) o;
+            weatherData.getTemperature();
+            weatherData.getHumidity();
             display();
         }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Temp: " + temperature + " Humidity: " + humidity);
     }
 }
--- java/src/WeatherStation3/Displays/ForecastDisplay.java
+++ java/src/WeatherStation3/Displays/ForecastDisplay.java
@@ -1,19 +1,31 @@
 package src.WeatherStation3.Displays;
 
 import src.WeatherStation3.Interfaces.DisplayElement;
+import src.WeatherStation3.WeatherData;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class ForecastDisplay implements Observer, DisplayElement {
+    private float currentPressure = 29.92f;
+    private float lastPressure;
+
+    public ForecastDisplay(Observable observable) {
+        observable.addObserver(this);
+    }
 
     @Override
     public void update(Observable o, Object arg) {
-
+        if (o instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData) o;
+            lastPressure = currentPressure;
+            currentPressure = weatherData.getPressure();
+            display();
+        }
     }
 
     @Override
     public void display() {
-
+        System.out.println("Current: " + currentPressure + " Last pressure: " + lastPressure);
     }
 }
