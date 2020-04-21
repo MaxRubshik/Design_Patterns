dbbddcea608d7241926941dd1025e7b5011086eb
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-21 15:18:54 +0300

    StarBuzz task start.

---
+++ java/src/SturBuzz/Beverage.java
@@ -0,0 +1,11 @@
+package src.SturBuzz;
+
+public abstract class Beverage {
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract int cost();
+}
---
+++ java/src/SturBuzz/Decaf.java
@@ -0,0 +1,8 @@
+package src.SturBuzz;
+
+public class Decaf extends Beverage {
+    @Override
+    public int cost() {
+        return 0;
+    }
+}
