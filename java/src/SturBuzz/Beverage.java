beece8e228851a46d37dd1d614128dd27973108d
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-25 10:48:28 +0300

    Add cost methods.

--- java/src/SturBuzz/Beverage.java
+++ java/src/SturBuzz/Beverage.java
@@ -2,11 +2,43 @@ package src.SturBuzz;
 
 public abstract class Beverage {
     public String description = "Unknown beverage.";
-    double milk;
-    double soy;
-    double mocha;
+    double milkCost;
+    double soyCost;
+    double mochaCost;
+    double whipCost;
+    double condimentCost = 0.0;
 
-    public abstract double cost();
+    public double cost() {
+        if (hasMilk()) {
+            condimentCost += milkCost;
+        }
+        if (hasSoy()) {
+            condimentCost += soyCost;
+        }
+        if (hasMocha()){
+            condimentCost += mochaCost;
+        }
+        if (hasWhip()){
+            condimentCost += whipCost;
+        }
+        return condimentCost;
+    }
+
+    private boolean hasWhip() {
+        return false;
+    }
+
+    private boolean hasMocha() {
+        return false;
+    }
+
+    private boolean hasSoy() {
+        return false;
+    }
+
+    private boolean hasMilk() {
+        return false;
+    }
 
     public String getDescription() {
         return description;
