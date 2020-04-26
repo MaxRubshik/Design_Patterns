9f270e4b296660a09e02c72c0683d8396399b1af
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-26 18:58:43 +0300

    InputStream App. Update.

---
+++ InputTest.java
@@ -0,0 +1,19 @@
+package src.LowerCase;
+
+import java.io.*;
+
+public class InputTest {
+    public static void main(String[] args) {
+        int c;
+        try {
+            InputStream in = new LowerCaseInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream("test.rtf")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
