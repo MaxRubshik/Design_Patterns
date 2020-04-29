e991faacbe3f1f9b77a9ff365992e2e296e7ce19
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-29 09:21:01 +0300

    Pizza class update.

--- java/src/Pizza/Pizza.java
+++ java/src/Pizza/Pizza.java
@@ -1,38 +1,59 @@
 package src.Pizza;
 
-import static src.Pizza.SimplePizzaFactory.createPizza;
+import java.util.ArrayList;
 
 public abstract class Pizza {
-    Pizza orderPizza(String type) {
-        Pizza pizza = createPizza(type);
-//        String name;
-//        String dough;
-//        String sauce;
+    String name;
+    String dough;
+    String sauce;
 
-        //ArrayList toppings = new ArrayList();
-
-
-        pizza.prepare();
-        pizza.bake();
-        pizza.cut();
-        pizza.box();
-
-        return pizza;
-    }
+    ArrayList toppings = new ArrayList();
 
     public void prepare() {
-        System.out.println("Prepare.");
+        System.out.println("Prepare " + name);
     }
 
     public void box() {
-        System.out.println("In the box");
+        System.out.println("In the box" + name);
     }
 
     public void cut() {
-        System.out.println("Cut");
+        System.out.println("Cut " + name);
     }
 
     public void bake() {
-        System.out.println("Bake");
+        System.out.println("Bake " + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDough() {
+        return dough;
+    }
+
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+
+    public String getSauce() {
+        return sauce;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+
+    public ArrayList getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(ArrayList toppings) {
+        this.toppings = toppings;
     }
 }
