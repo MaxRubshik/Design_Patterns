b2224e0d4e8c010a72f01dffdbc99a6c82c28b58
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-04-30 14:23:12 +0300

    abstract Pizza update.

--- java/src/Pizza/PizzaVersions/Pizza.java
+++ java/src/Pizza/PizzaVersions/Pizza.java
@@ -1,68 +1,79 @@
 package src.Pizza.PizzaVersions;
 
+import src.Pizza.Ingredients.*;
+
 import java.util.ArrayList;
 
 public abstract class Pizza {
     protected String name;
-    protected String dough;
-    protected String sauce;
+    protected Dough dough;
+    protected Sauce sauce;
+    protected Veggies veggies[];
+    protected Cheese cheese;
+    protected Pepperoni pepperoni;
+    protected Clams clams;
 
     ArrayList toppings = new ArrayList();
 
-    public void prepare() {
+    abstract void prepare();
+//    {
+//
+//        System.out.println("Prepare " + name);
+//        System.out.println("Tossing dough...");
+//        System.out.println("Adding sauce...");
+//        System.out.println("Adding toppings...");
+//        System.out.println();
+//
+//        for (int i = 0; i < toppings.size(); i++) {
+//            System.out.println(" "+ toppings.get(i));
+//        }
+//    }
+    public void bake() {
 
-        System.out.println("Prepare " + name);
-        System.out.println("Tossing dough...");
-        System.out.println("Adding sauce...");
-        System.out.println("Adding toppings...");
-        System.out.println();
-
-        for (int i = 0; i < toppings.size(); i++) {
-            System.out.println(" "+ toppings.get(i));
-        }
+        System.out.println("Bake 25 minutes. ");
+    }
+    public void cut() {
+        System.out.println("Cutting into diagonal pieces. ");
     }
 
     public void box() {
-        System.out.println("In the box " + name);
-    }
-
-    public void cut() {
-        System.out.println("Cut " + name);
-    }
-
-    public void bake() {
-        System.out.println("Bake " + name);
-    }
-
-    public String getName() {
-        return name;
+        System.out.println("Put in the official box. ");
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getDough() {
-        return dough;
+    public String getName() {
+        return name;
     }
 
-    public void setDough(String dough) {
-        this.dough = dough;
+    @Override
+    public String toString() {
+        return "Pizza info";
     }
 
-    public String getSauce() {
-        return sauce;
-    }
-
-    public void setSauce(String sauce) {
-        this.sauce = sauce;
-    }
-
-    public ArrayList getToppings() {
-        return toppings;
-    }
-
-    public void setToppings(ArrayList toppings) {
-        this.toppings = toppings;
-    }
+//    public String getDough() {
+//        return dough;
+//    }
+//
+//    public void setDough(String dough) {
+//        this.dough = dough;
+//    }
+//
+//    public String getSauce() {
+//        return sauce;
+//    }
+//
+//    public void setSauce(String sauce) {
+//        this.sauce = sauce;
+//    }
+//
+//    public ArrayList getToppings() {
+//        return toppings;
+//    }
+//
+//    public void setToppings(ArrayList toppings) {
+//        this.toppings = toppings;
+//    }
 }
