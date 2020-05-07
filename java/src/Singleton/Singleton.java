6bad0687068aef4c38faa8afc8c24ec9213a38a6
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-07 15:17:13 +0300

    synchronized.

--- java/src/Singleton/Singleton.java
+++ java/src/Singleton/Singleton.java
@@ -3,11 +3,13 @@ package src.Singleton;
 public class Singleton {
     private static Singleton uniqueInstance;
 
-    private Singleton(){};
+    private Singleton() {
+    }
 
-    public static void main(String[] args) {
-        if (uniqueInstance == null){
+    public static synchronized Singleton getInstance() {
+        if (uniqueInstance == null) {
             uniqueInstance = new Singleton();
         }
+        return uniqueInstance;
     }
 }
