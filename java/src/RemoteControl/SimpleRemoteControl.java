a8dee41b42f9a67d383924ffb7095936488f5386
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-14 12:20:14 +0300

    SimpleRemoteControl. Added.

--- java/src/RemoteControl/SimpleRemoteControl.java
+++ java/src/RemoteControl/SimpleRemoteControl.java
@@ -2,4 +2,16 @@ package src.RemoteControl;
 
 public class SimpleRemoteControl {
     Command slot;
+
+    public SimpleRemoteControl(){
+
+    }
+
+    public void setCommand(Command command){
+        slot = command;
+    }
+
+    public void buttonWasPressed(){
+        slot.execute();
+    }
 }
