48ecfd15d3b48eb6785eb027675da94dd3c83ae2
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-14 14:58:17 +0300

    GarageDoor test. Added.

--- java/src/RemoteControl/RemoteControlTest.java
+++ java/src/RemoteControl/RemoteControlTest.java
@@ -4,9 +4,15 @@ public class RemoteControlTest {
     public static void main(String[] args) {
         SimpleRemoteControl remote = new SimpleRemoteControl();
         Light light = new Light();
+
+        GarageDoor garageDoor = new GarageDoor();
         LightOnCommand lightOn = new LightOnCommand(light);
 
+        GarageDoorOpenComand garageOpen = new GarageDoorOpenComand(garageDoor);
+
         remote.setCommand(lightOn);
+        remote.buttonWasPressed();
+        remote.setCommand(garageOpen);
         remote.buttonWasPressed();
     }
 }
