608d4abfed27a55dd360c01c522752833a530bac
«MaxRubshik» <rubshikmax@gmail.com>
2020-05-15 12:14:02 +0300

    RemoteControl started.

---
+++ java/src/RemoteControl2/RemoteControl.java
@@ -0,0 +1,20 @@
+package src.RemoteControl2;
+
+public class RemoteControl {
+//    Command[] onCommands;
+//    Command[] offCommands;
+//
+//    public RemoteControl(){
+//        onCommands = new Command[7];
+//        offCommands = new Command[7];
+//
+//        Command noCommand = new NoCommand();
+//
+//        for (int i = 0; i < 7; i++) {
+//            onCommands[i] = noCommand;
+//            offCommands[i] = noCommand;
+//        }
+//    }
+//
+//    public void setCommand(){}
+}
