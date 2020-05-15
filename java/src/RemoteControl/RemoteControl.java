7c238f2bd8a50cb54e082cfb18aaa5a6fc5510b8
«MaxRubshik» <rubshikmax@gmail.com>
2020-05-15 12:30:13 +0300

    RemoteControl toString.

--- java/src/RemoteControl/RemoteControl.java
+++ java/src/RemoteControl/RemoteControl.java
@@ -15,8 +15,26 @@ public class RemoteControl {
         }
     }
 
-    public void setCommand(int slot, Command onCommand, Command offCommand){
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
         onCommands[slot] = onCommand;
         offCommands[slot] = offCommand;
+    }
+
+    public void onButtonWasPushed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPressed(int slot) {
+        offCommands[slot].execute();
+    }
+
+    public String toString() {
+        StringBuffer stringBuff = new StringBuffer();
+        stringBuff.append("\n------Remote Control ------\n");
+        for (int i = 0; i < onCommands.length; i++) {
+            stringBuff.append("[slot " + i + "] " + onCommands[i].getClass().getName()
+                    + "  " + offCommands[i].getClass().getName() + "\n");
+        }
+        return stringBuff.toString();
     }
 }
