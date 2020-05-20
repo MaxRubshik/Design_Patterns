040d92f93d25c1c92c00b1c636855d489dc6d7d9
«MaxRubshik» <rubshikmax@gmail.com>
2020-05-20 11:44:49 +0300

    turkey adapter

---
+++ java/src/DuckPond3/DuckTestDrive.java
@@ -0,0 +1,8 @@
+package src.DuckPond3;
+
+public class DuckTestDrive {
+    public static void main(String[] args) {
+        MallardDuck mallardDuck = new MallardDuck();
+        WildTurkey wildTurkey = new WildTurkey();
+    }
+}
