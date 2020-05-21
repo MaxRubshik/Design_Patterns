f5fbd1105e3b036518ccf0cdfba9f378bad189c7
«MaxRubshik» <rubshikmax@gmail.com>
2020-05-21 15:17:44 +0300

    Iterator from Iterator to Enumeration

---
+++ java/src/EnumerationTask/IteratorMy.java
@@ -0,0 +1,22 @@
+package src.EnumerationTask;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class IteratorMy implements Enumeration {
+    Iterator iterator;
+
+    public IteratorMy(Iterator iterator) {
+        this.iterator = iterator;
+    }
+
+    @Override
+    public boolean hasMoreElements() {
+        return iterator.hasNext();
+    }
+
+    @Override
+    public Object nextElement() {
+        return iterator.next();
+    }
+}
