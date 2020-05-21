0aa5fe51685d05004f95cda202163fb730dbb269
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-21 19:55:14 +0300

    Phibonacci

---
+++ java/src/EnumerationTask/Fibo.java
@@ -0,0 +1,12 @@
+package src.EnumerationTask;
+
+public class Fibo {
+    public static void main(String[] args) {
+        printFibonacci(3);
+    }
+    static void printFibonacci(long n){
+        // enter your code
+        System.out.println(n );
+    }
+
+}
