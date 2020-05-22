474661134e8da9564e9c5eaf47b24a78f915ed3b
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-22 12:46:23 +0300

    HomeTheaterTest class.

--- java/src/HomeTheater/HomeTheaterTest.java
+++ java/src/HomeTheater/HomeTheaterTest.java
@@ -2,9 +2,18 @@ package src.HomeTheater;
 
 public class HomeTheaterTest {
     public static void main(String[] args) {
+        Amplifier amp = new Amplifier();
+        Tuner tuner = new Tuner();
+        DvdPlayer dvd = new DvdPlayer();
+        CdPlayer cd = new CdPlayer();
+        Projector projector = new Projector();
+        Screen screen = new Screen();
+        TheaterLights lights = new TheaterLights();
+        PopcornPopper popper = new PopcornPopper();
+
         HomeTheaterFacade homeTheater =
                 new HomeTheaterFacade(amp, tuner, dvd, cd, projector,
-                        screen, lights, popper);
+                        lights, screen, popper);
 
         homeTheater.watchMovie("Adrenalin");
         homeTheater.endMovie();
