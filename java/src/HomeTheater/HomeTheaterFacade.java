2a1a6c9cfcd53d7f4bddc5456dd6da8dff9a90dc
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-22 12:14:05 +0300

    HomeTheater

---
+++ java/src/HomeTheater/HomeTheaterFacade.java
@@ -0,0 +1,23 @@
+package src.HomeTheater;
+
+public class HomeTheaterFacade {
+    Amplifier amp;
+    Tuner tuner;
+    DvdPlayer dvd;
+    CdPlayer cd;
+    Projector projector;
+    TheaterLights lights;
+    Screen screen;
+    PopcornPopper popper;
+
+    public HomeTheaterFacade(Amplifier amp, Tuner tuner, DvdPlayer dvd, CdPlayer cd, Projector projector, TheaterLights lights, Screen screen, PopcornPopper popper) {
+        this.amp = amp;
+        this.tuner = tuner;
+        this.dvd = dvd;
+        this.cd = cd;
+        this.projector = projector;
+        this.lights = lights;
+        this.screen = screen;
+        this.popper = popper;
+    }
+}
---
+++ java/src/HomeTheater/TheaterLights.java
@@ -0,0 +1,4 @@
+package src.HomeTheater;
+
+public class TheaterLights {
+}
