e4994bc0c5edb0942952a2fd6629974e91c5c41a
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-23 00:20:47 +0300

    calculateAndPrint() process.

---
+++ java/src/EnumerationTask/CalculateAndPrint.java
@@ -0,0 +1,21 @@
+package src.EnumerationTask;
+
+public class CalculateAndPrint {
+    public static void main(String[] args) {
+        calculateAndPrint(1211);
+        calculateAndPrint(2222);
+        calculateAndPrint(1311);
+    }
+
+    private static void calculateAndPrint(int n) {
+        if (n > 1000 && n < 10000) {
+            if (n / 100 % 2 == 0) {
+                System.out.println(n + 100);
+
+            } else {
+                n = n * 2;
+                System.out.println(n);
+            }
+        }
+    }
+}
--- java/src/EnumerationTask/Fibo.java
+++ java/src/EnumerationTask/Fibo.java
@@ -2,11 +2,33 @@ package src.EnumerationTask;
 
 public class Fibo {
     public static void main(String[] args) {
-        printFibonacci(3);
+        printFibonacci(11);
     }
-    static void printFibonacci(long n){
+
+    //    static long printFibonacci(long n) {
+//        // enter your code
+//        if (n <= 1) {
+//            return n;
+//        } else {
+//            return printFibonacci(n-1)+ printFibonacci(n-2);
+//        }
+//    }
+    static void printFibonacci(long n) {
         // enter your code
-        System.out.println(n );
+        long sum = 0;
+        if (n <= 1) {
+            System.out.println(n);
+        } else {
+            //long a = printFibonacci(n - 1) + printFibonacci(n - 2);
+            long a = 1;
+            long b = 1;
+            sum = a + b;
+            while (sum < n) {
+                a = b;
+                b = sum;
+            }
+        }
+        System.out.println(sum);
     }
 
 }
