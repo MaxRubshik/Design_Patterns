fd41bc36371a6d1917c55a21af7fa8ecd593dce8
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-27 11:57:05 +0300

    Duck class. compareTo().

---
+++ java/src/sort/Duck.java
@@ -0,0 +1,29 @@
+package src.sort;
+
+public class Duck implements Comparable {
+    String name;
+    int weight;
+
+    @Override
+    public String toString() {
+        return name + " weight " + weight;
+    }
+
+    public Duck(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Object object) {
+        Duck otherDuck = (Duck) object;
+
+        if(this.weight < otherDuck.weight){
+            return -1;
+        } else if (this.weight == otherDuck.weight){
+            return 0;
+        } else { //this.weight > otherDuck
+            return 1;
+        }
+    }
+}
