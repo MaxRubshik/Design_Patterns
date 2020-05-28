24ed80260b4ae86b887d5b78a8989bc44649eeff
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-28 11:18:27 +0300

    PanCakeHouseMenu added.

---
+++ java/src/MenuItem/PancakeHouseMenu.java
@@ -0,0 +1,37 @@
+package src.MenuItem;
+
+import java.util.ArrayList;
+
+public class PancakeHouseMenu {
+    ArrayList menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList();
+
+        addItem("K&B Pancake breakfast",
+                "Pancakes with scrambled eggs and toast",
+                true, 2.99);
+
+        addItem("Regular Pancake Breakfast", "Pancake with fried eggs, sausage",
+                false, 2.99);
+
+        addItem("BlueBerry Pancakes", "Pancakes made with fresh blueberries",
+                true, 3.49);
+
+        addItem("Waffles", "Waffles with your choice of blueberries or strawberries",
+                true, 3.593);
+    }
+
+    private void addItem(String name, String description,
+                         boolean vegetarian, double price) {
+
+        MenuItem menuItem = new MenuItem(name, description,
+                vegetarian, price);
+
+        menuItems.add(menuItem);
+    }
+
+    public ArrayList getMenuItems() {
+        return menuItems;
+    }
+}
