19bcd01b2e097d6f5ab21d6bca479d5993f3a346
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-05-28 10:24:56 +0300

    MenuItem added.

--- java/src/sort/MyApplet.java
+++ java/src/sort/MyApplet.java
@@ -1,7 +1,30 @@
 package src.sort;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class MyApplet extends Applet {
     String message;
+
+    public void init(){
+        message = "Hello World, I'm alive!";
+        repaint();
+    }
+
+    public void start(){
+        message = "Now I'm starting up!";
+        repaint();
+    }
+
+    public void stop(){
+        message = "Oh, now I'm being stopped...";
+    }
+
+    public void destroy(){
+        //applet destroying...
+    }
+
+    public void paint(Graphics g){
+        g.drawString(message, 5 ,15);
+    }
 }
