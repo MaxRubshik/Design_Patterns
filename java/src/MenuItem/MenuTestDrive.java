d3db8b4cdd9364e707db3d937a90b4eb5eff2395
«MaxRubshik» <rubshikmax@gmail.com>
2020-05-29 09:50:13 +0300

    MenuTestDrive added.

---
+++ java/src/MenuItem/MenuTestDrive.java
@@ -0,0 +1,12 @@
+package src.MenuItem;
+
+public class MenuTestDrive {
+    public static void main(String[] args) {
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinerMenu dinerMenu = new DinerMenu();
+
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+
+        waitress.printMenu();
+    }
+}
