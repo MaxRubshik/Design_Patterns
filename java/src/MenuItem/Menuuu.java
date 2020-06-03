aba7602cac109098cb09eba625cd0c8939c323de
«MaxRubshik» <rubshikmax@gmail.com>
2020-06-03 12:55:43 +0300

    Menuu. Combined node.

---
+++ java/src/MenuItem/Menuuu.java
@@ -0,0 +1,43 @@
+package src.MenuItem;
+
+import java.util.ArrayList;
+
+public class Menuuu extends MenuComponent {
+    ArrayList menuComponents = new ArrayList();
+    String name;
+    String description;
+
+    public Menuuu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    public void remove(MenuComponent menuComponent){
+        menuComponents.remove(menuComponent);
+    }
+
+    public MenuComponent getChild(int i){
+        return (MenuComponent) menuComponents.get(i);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String getDescription() {
+        return getDescription();
+    }
+
+    @Override
+    public void print() {
+        System.out.print("\n "+ getName());
+        System.out.println("\n "+ getDescription());
+        System.out.println("-----------------------");
+    }
+}
