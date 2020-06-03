0eddf67242841a9dc9657ccbc74e9a7345f7ae6a
«MaxRubshik» <rubshikmax@gmail.com>
2020-06-03 13:54:01 +0300

    testDrive

--- java/src/MenuItem/MenuTestDrive.java
+++ java/src/MenuItem/MenuTestDrive.java
@@ -2,12 +2,42 @@ package src.MenuItem;
 
 public class MenuTestDrive {
     public static void main(String[] args) {
-        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
-        DinerMenu dinerMenu = new DinerMenu();
-        CafeMenu cafeMenu = new CafeMenu();
+        MenuComponent pancakeHouseMenu =
+                new Menuuu("Pancake House Menu", "Breakfast");
+        MenuComponent dinerMenu =
+                new Menuuu("DINER MENU", "Launch");
+        MenuComponent cafeMenu =
+                new Menuuu("CAFE MENU", "Dinner");
+        MenuComponent dessertMenu =
+                new Menuuu("DESSERT MENU", "Dessert of course!");
 
-        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
 
+        MenuComponent allMenus =
+                new Menuuu("ALL MENUS", "All Menus Comdined");
+
+        allMenus.add(pancakeHouseMenu);
+        allMenus.add(dinerMenu);
+        allMenus.add(cafeMenu);
+        allMenus.add(dessertMenu);
+
+        dinerMenu.add(new MenuItem(
+                "Pasta",
+                "Spaghetti with Marinara",
+                true,
+                3.89
+        ));
+
+        dinerMenu.add(dessertMenu);
+
+        dessertMenu.add(new MenuItem(
+                        "Apple Pie",
+                        "Apple with flakey crust",
+                        true,
+                        1.59
+                )
+        );
+
+        Waitress waitress = new Waitress(allMenus);
         waitress.printMenu();
     }
 }
