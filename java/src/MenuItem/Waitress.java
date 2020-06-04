ddf83b69de566a8cf9f56de42f098321766e3a09
«MaxRubshik» <rubshikmax@gmail.com>
2020-06-04 11:58:12 +0300

    Waitress update

--- java/src/MenuItem/Waitress.java
+++ java/src/MenuItem/Waitress.java
@@ -13,11 +13,17 @@ public class Waitress {
         allMenus.print();
     }
 
-    public void printVegetarianMenu(){
+    public void printVegetarianMenu() {
         Iterator iterator = allMenus.createIterator();
         System.out.println("\nVEGETARIAN MENU\n");
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             MenuComponent menuComponent = (MenuComponent) iterator.next();
+            try {
+                if (menuComponent.isVegetarian()) {
+                    menuComponent.print();
+                }
+            } catch (UnsupportedOperationException e) {
+            }
         }
     }
 }
