258e0b1e99a9f245c5ef121e9578a80c582ad19b
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-08 16:14:25 +0300

    SoldOutState

--- java/src/BallMachine/SoldOutState.java
+++ java/src/BallMachine/SoldOutState.java
@@ -1,8 +1,29 @@
 package src.BallMachine;
 
-public class SoldOutState {
+public class SoldOutState implements State {
     GumballMachine gumballMachine;
+
     public SoldOutState(GumballMachine gumballMachine) {
         this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("Ypu can't insert. It's empty");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("No coins here.");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("No coins and balls.");
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("No gumballs dispensed.");
     }
 }
