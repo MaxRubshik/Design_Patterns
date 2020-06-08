f2698127bde366cecc73b04a431e88f48877b50b
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-08 15:11:01 +0300

    HasQuarterState

---
+++ java/src/BallMachine/HasQuarterState.java
@@ -0,0 +1,31 @@
+package src.BallMachine;
+
+public class HasQuarterState implements  State {
+    GumballMachine gumballMachine;
+
+    public HasQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("You can't insert another  coin.");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("Coin returned.");
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("You turned...");
+        gumballMachine.setState(gumballMachine.getSoldState());
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("No gumball dispensed.");
+    }
+}
