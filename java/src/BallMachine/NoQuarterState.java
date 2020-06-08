35a9167cb7a8e3e851bc75e390dc7b167c9cbb11
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-08 13:05:28 +0300

    NoQuarterState

--- java/src/BallMachine/NoQuarterState.java
+++ java/src/BallMachine/NoQuarterState.java
@@ -2,4 +2,29 @@ package src.BallMachine;
 
 public class NoQuarterState implements State {
     GumballMachine gumballMachine;
+
+    public NoQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("You inserted coin");
+        gumballMachine.getState(gumballMachine.getHasQuarterState());
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("You haven't inserted a coin");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("You turned, but there's no coin.");
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("You need to pay first.");
+    }
 }
