38f25073d1a2fe750189a2e33317fc8d31399841
«MaxRubshik» <rubshikmax@gmail.com>
2020-06-09 11:45:50 +0300

    WinnerState class added.

---
+++ java/src/BallMachine/WinnerState.java
@@ -0,0 +1,42 @@
+package src.BallMachine;
+
+public class WinnerState implements State {
+    GumballMachine gumballMachine;
+
+    public WinnerState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("Please wait, we're already giving you a gumball.");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("Sorry, you're already turned the crank.");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("Turning twice, doesn't giving you another ball!");
+    }
+
+
+    @Override
+    public void dispense() {
+        System.out.println("YOU'RE A WINNER! You get two balls for your coin!");
+        gumballMachine.releaseBall();
+        if (gumballMachine.getCount() == 0) {
+            gumballMachine.setState(gumballMachine.soldOutState);
+        } else {
+            gumballMachine.releaseBall();
+            if (gumballMachine.getCount() > 0) {
+                gumballMachine.setState(gumballMachine.getNoQuarterState());
+            } else {
+                System.out.println("Oops! Out of balls.");
+                gumballMachine.setState(gumballMachine.getSoldOutState());
+            }
+        }
+    }
+}
