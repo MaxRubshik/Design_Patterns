567592aca6d9197f80a7a11c3204281859baefbf
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-15 11:09:50 +0300

    MyRemoteImpl update.

--- java/src/Remote/MyRemoteImpl.java
+++ java/src/Remote/MyRemoteImpl.java
@@ -1,14 +1,23 @@
 package src.Remote;
 
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
+import java.rmi.*;
+import java.rmi.server.*;
 
 public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
     public MyRemoteImpl() throws RemoteException {
     }
 
     @Override
-    public String sayHello() throws RemoteException {
-        return null;
+    public String sayHello() {
+        return "Server says, 'Hey'";
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyRemote service = new MyRemoteImpl();
+            Naming.rebind("RemoteHello", service);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
