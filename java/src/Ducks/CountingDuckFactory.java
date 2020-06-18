2a52877318c374bf3d2c5d12167d296b94560aae
«MaxRubshik» <rubshikmax@gmail.com>
2020-06-18 13:59:14 +0300

    CountingDuckFactory

---
+++ java/src/Ducks/CountingDuckFactory.java
@@ -0,0 +1,23 @@
+package src.Ducks;
+
+public class CountingDuckFactory extends AbstractDuckFactory {
+    @Override
+    public Quackable createMallardDuck() {
+        return new QuackCounter(new MallardDuck());
+    }
+
+    @Override
+    public Quackable createRedHeadDuck() {
+        return new QuackCounter(new RubberDuck());
+    }
+
+    @Override
+    public Quackable createDuckCall() {
+        return new QuackCounter(new DuckCall());
+    }
+
+    @Override
+    public Quackable createRubberDuck() {
+        return new QuackCounter(new RubberDuck());
+    }
+}
