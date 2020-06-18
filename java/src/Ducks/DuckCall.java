3c165a32327023a57960c30872c9299e4e38a586
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-18 06:26:51 +0300

    DuckCall and RubberDuck

---
+++ java/src/Ducks/DuckCall.java
@@ -0,0 +1,8 @@
+package src.Ducks;
+
+public class DuckCall implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Kwak");
+    }
+}
---
+++ java/src/Ducks/RubberDuck.java
@@ -0,0 +1,8 @@
+package src.Ducks;
+
+public class RubberDuck implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Squeek");
+    }
+}
