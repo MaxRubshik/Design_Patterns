1cd2050c8874756bb033c1fae24b63928e23c92b
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-22 15:51:14 +0300

    Flock update with register

--- java/src/Ducks/FLock.java
+++ java/src/Ducks/FLock.java
@@ -2,12 +2,14 @@ package src.Ducks;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Observer;
 
 public class FLock implements Quackable {
     ArrayList quackers = new ArrayList();
 
     public void add(Quackable quacker){
         quackers.add(quacker);
+        notifyObservers();
     }
 
     @Override
@@ -17,5 +19,19 @@ public class FLock implements Quackable {
             Quackable quacker = (Quackable) iterator.next();
             quacker.quack();
         }
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        Iterator iterator = quackers.iterator();
+        while (iterator.hasNext()){
+            Quackable quacker = (Quackable) iterator.next();
+            quacker.registerObserver(observer);
+        }
+    }
+
+    @Override
+    public void notifyObservers() {
+
     }
 }
