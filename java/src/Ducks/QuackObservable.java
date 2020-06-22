366f0d84dacd13947bfc99be0075bb45506028bc
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-22 10:20:32 +0300

    interface QuackObservable update

--- java/src/Ducks/QuackObservable.java
+++ java/src/Ducks/QuackObservable.java
@@ -1,6 +1,6 @@
 package src.Ducks;
 
-import src.WeatherStation.interfaces.Observer;
+import java.util.Observer;
 
 public interface QuackObservable {
     public void registerObserver(Observer observer);
