b1cbe08ce34acecc72f7f4219f9994eb69ca8c0d
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-23 19:28:46 +0300

    BeatModel lots of methods

--- java/src/Midi/BeatModel.java
+++ java/src/Midi/BeatModel.java
@@ -1,46 +1,112 @@
 package src.Midi;
 
-import javax.sound.midi.MetaEventListener;
-import javax.sound.midi.MetaMessage;
+import javax.sound.midi.*;
+import java.util.ArrayList;
 
 public class BeatModel implements BeatModeInterface, MetaEventListener {
-    @Override
-    public void meta(MetaMessage meta) {
+    Sequencer sequencer;
+    ArrayList beatObservers = new ArrayList();
+    ArrayList bpmObservers = new ArrayList();
+    int bpm = 90;
+    Sequence sequence;
+    Track track;
 
+    @Override
+    public void meta(MetaMessage message) {
+        if (message.getType() == 47) {
+            beatEvent();
+            sequencer.start();
+            setBPM(getBPM());
+        }
     }
 
     @Override
     public void initialize() {
+        setUpMidi();
+        buildTrackAndStart();
+    }
 
+    private void buildTrackAndStart() {
     }
 
     @Override
     public void on() {
-
+        sequencer.start();
+        setBPM(90);
     }
 
     @Override
     public void off() {
-
+        setBPM(0);
+        sequencer.stop();
     }
 
     @Override
     public void setBPM(int bpm) {
+        this.bpm = bpm;
+        sequencer.setTempoInBPM(getBPM());
+        notifyBPMObservers();
+    }
 
+    void beatEvent() {
+        notifyBeatObservers();
     }
 
     @Override
     public int getBPM() {
-        return 0;
+        return bpm;
+    }
+
+    @Override
+    public void registerObserver(BPMObserver o) {
+        bpmObservers.add(o);
     }
 
     @Override
     public void registerObserver(BeatObserver o) {
+        beatObservers.add(o);
+    }
 
+    @Override
+    public void removeObserver(BeatObserver o) {
+        int i = beatObservers.indexOf(o);
+        if (i >= 0) {
+            beatObservers.remove(i);
+        }
     }
 
     @Override
     public void removeObserver(BPMObserver o) {
+        int i = bpmObservers.indexOf(o);
+        if (i >= 0) {
+            bpmObservers.remove(i);
+        }
+    }
 
+    public void notifyBeatObservers() {
+        for (int i = 0; i < beatObservers.size(); i++) {
+            BeatObserver observer = beatObservers.get(i);
+            observer.updateBeat();
+        }
+    }
+
+    public void notifyBPMObservers() {
+        for (int i = 0; i < bpmObservers.size(); i++) {
+            BPMObserver observer = bpmObservers.get(i);
+            observer.updateBeat();
+        }
+    }
+
+    private void setUpMidi() {
+        try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequencer.addMetaEventListener(this);
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(getBPM());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
