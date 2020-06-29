3be926859c4003cd51ca229e11bde148244d803a
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-06-29 12:19:34 +0300

    HeartModel and Interface

---
+++ java/src/Midi/HeartTestDrive.java
@@ -0,0 +1,8 @@
+package src.Midi;
+
+public class HeartTestDrive {
+    public static void main(String[] args) {
+        HeartModel heartModel = new HeartModel();
+        ControllerInterface model = new HeartCOntroller(heartModel);
+    }
+}
